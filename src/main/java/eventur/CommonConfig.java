ff2dd4a1a288f2c2bb2058ca912b04df6e15d5a0
syntel <syntel@PHX440G3-2815Z9.syntelorg.com>
2018-04-04 14:53:37 -0400

    switched to h2

--- src/main/java/eventur/CommonConfig.java
+++ src/main/java/eventur/CommonConfig.java
@@ -16,7 +16,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @Configuration
 public class CommonConfig {
 
- @Qualifier("dataSource")
+// @Qualifier("dataSource")
  @Autowired
  DataSource dataSource;
 
